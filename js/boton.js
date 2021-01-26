db9adec0ede8dd28995f588f33b593ba98294ccc
nelson <yardanicalderon@gmail.com>
2021-01-26 00:17:11 -0500

    pagina de servicios y nosotros finalizadas

---
+++ js/boton.js
@@ -0,0 +1,77 @@
+
+function openlab() {
+    document.getElementById("lab").style.width = "100%";
+  }
+
+  function closetlab() {
+    document.getElementById("lab").style.width = "0";
+  }
+
+  function openper() {
+    document.getElementById("per").style.width = "100%";
+  }
+
+  function closetper() {
+    document.getElementById("per").style.width = "0";
+  }
+
+  function opencon() {
+    document.getElementById("con").style.width = "100%";
+  }
+
+  function closetcon() {
+    document.getElementById("con").style.width = "0";
+  }
+
+
+  function opengeo() {
+    document.getElementById("geo").style.width = "100%";
+  }
+
+  function closetgeo() {
+    document.getElementById("geo").style.width = "0";
+  }
+
+
+  function opennos() {
+    document.getElementById("nos").style.height = "100%";
+  }
+
+  function closetnos() {
+    document.getElementById("nos").style.height = "0";
+  }
+
+  
+  function opennosr() {
+    document.getElementById("nosr").style.height = "100%";
+  }
+
+  function closetnosr() {
+    document.getElementById("nosr").style.height = "0";
+  }
+
+
+  function openname() {
+    document.getElementById("name").style.transform = "rotateY(180deg)";
+  }
+
+  function closetname() {
+    document.getElementById("name").style.transform = "rotateY(0deg)";
+  }
+
+
+  function openname2() {
+    document.getElementById("name2").style.transform = "rotateY(180deg)";
+  }
+
+  function closetname2() {
+    document.getElementById("name2").style.transform = "rotateY(0deg)";
+  }
+
+  function openname3() {
+    document.getElementById("name3").style.transform = "rotateY(180deg)";
+  }
+
+  function closetname3() {
+    document.getElementById("name3").style.transform = "rotateY(0deg)";
+  }
